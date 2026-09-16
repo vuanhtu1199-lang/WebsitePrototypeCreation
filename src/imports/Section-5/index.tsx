b219df5b2561e02a920cde5fa6d05fc6d37c6eeb
sboxd <sbox@figma.com>
2026-09-16 02:58:39 +0000

    fs write: 7rshZN20lRhYXDtO

---
+++ src/imports/Section-5/index.tsx
@@ -0,0 +1,340 @@
+import svgPaths from "./svg-a0at9qd4oc";
+import imgImageDavidLansdellDirector from "./3849eacacaa613d286caca5a991e26fa60175bda.png";
+
+function ImageDavidLansdellDirector() {
+  return (
+    <div className="h-[445.977px] relative shrink-0 w-full" data-name="Image (David Lansdell - Director)">
+      <img alt="" className="absolute inset-0 max-w-none object-cover pointer-events-none size-full" src={imgImageDavidLansdellDirector} />
+    </div>
+  );
+}
+
+function Container1() {
+  return (
+    <div className="bg-[#0a1b3a] col-1 content-stretch flex flex-col items-start justify-self-stretch overflow-clip relative row-1 self-stretch shrink-0" data-name="Container">
+      <ImageDavidLansdellDirector />
+    </div>
+  );
+}
+
+function Icon() {
+  return (
+    <div className="relative shrink-0 size-[8px]" data-name="Icon">
+      <svg className="absolute block inset-0 size-full" fill="none" height="8" preserveAspectRatio="none" viewBox="0 0 8 8" width="8">
+        <g clipPath="url(#clip0_0_12)" id="Icon">
+          <path d="M8 0H0V8H8V0Z" fill="#E5B869" id="Vector" />
+        </g>
+        <defs>
+          <clipPath id="clip0_0_12">
+            <rect fill="white" height="8" width="8" />
+          </clipPath>
+        </defs>
+      </svg>
+    </div>
+  );
+}
+
+function IconMargin() {
+  return (
+    <div className="content-stretch flex items-start pt-[4.9px] relative shrink-0" data-name="Icon:margin">
+      <Icon />
+    </div>
+  );
+}
+
+function Paragraph() {
+  return (
+    <div className="content-stretch flex gap-[10px] h-[38px] items-start pb-[18px] relative shrink-0 w-[170.32px]" data-name="Paragraph">
+      <IconMargin />
+      <p className="[word-break:break-word] font-['DM_Mono:Medium',sans-serif] leading-[19.6px] not-italic relative shrink-0 text-[14px] text-black tracking-[0.56px] uppercase whitespace-nowrap">Executive Profile</p>
+    </div>
+  );
+}
+
+function Heading() {
+  return (
+    <div className="content-stretch flex flex-col h-[57px] items-start pb-[18px] relative shrink-0 w-[206px]" data-name="Heading 2">
+      <p className="[word-break:break-word] font-['Onest:Regular',sans-serif] font-normal leading-[38.4px] relative shrink-0 text-[#111827] text-[32px] tracking-[-0.8px] whitespace-nowrap">David Lansdell</p>
+    </div>
+  );
+}
+
+function ContainerMargin() {
+  return (
+    <div className="content-stretch flex flex-col items-start pb-[16px] relative shrink-0" data-name="Container:margin">
+      <p className="[word-break:break-word] font-['Onest:SemiBold',sans-serif] font-semibold leading-[24.3px] relative shrink-0 text-[#e5b869] text-[18px] w-[285px]">{`Director — Construct Queensland `}</p>
+    </div>
+  );
+}
+
+function Icon1() {
+  return (
+    <div className="relative shrink-0 size-[18px]" data-name="Icon">
+      <svg className="absolute block inset-0 size-full" fill="none" height="18" preserveAspectRatio="none" viewBox="0 0 18 18" width="18">
+        <g id="Icon">
+          <path d={svgPaths.p27a39400} fill="#5B6574" id="Vector" />
+        </g>
+      </svg>
+    </div>
+  );
+}
+
+function Text() {
+  return (
+    <div className="content-stretch flex flex-col items-start relative shrink-0" data-name="Text">
+      <p className="[word-break:break-word] font-['Onest:SemiBold',sans-serif] font-semibold leading-[24px] relative shrink-0 text-[#0a1b3a] text-[16px] whitespace-nowrap">Executive Director (QLD Gov)</p>
+    </div>
+  );
+}
+
+function Container4() {
+  return (
+    <div className="col-1 content-stretch flex gap-[10px] items-center justify-self-stretch relative row-1 self-stretch shrink-0" data-name="Container">
+      <Icon1 />
+      <Text />
+    </div>
+  );
+}
+
+function Icon2() {
+  return (
+    <div className="relative shrink-0 size-[18px]" data-name="Icon">
+      <svg className="absolute block inset-0 size-full" fill="none" height="18" preserveAspectRatio="none" viewBox="0 0 18 18" width="18">
+        <g id="Icon">
+          <path d={svgPaths.p27a39400} fill="#5B6574" id="Vector" />
+        </g>
+      </svg>
+    </div>
+  );
+}
+
+function Text1() {
+  return (
+    <div className="content-stretch flex flex-col items-start relative shrink-0" data-name="Text">
+      <p className="[word-break:break-word] font-['Onest:SemiBold',sans-serif] font-semibold leading-[24px] relative shrink-0 text-[#0a1b3a] text-[16px] w-[257px]">Lendlease Senior Leadership Alumnus</p>
+    </div>
+  );
+}
+
+function Container5() {
+  return (
+    <div className="col-1 content-stretch flex gap-[10px] items-center justify-self-stretch relative row-2 self-stretch shrink-0" data-name="Container">
+      <Icon2 />
+      <Text1 />
+    </div>
+  );
+}
+
+function Icon3() {
+  return (
+    <div className="relative shrink-0 size-[18px]" data-name="Icon">
+      <svg className="absolute block inset-0 size-full" fill="none" height="18" preserveAspectRatio="none" viewBox="0 0 18 18" width="18">
+        <g id="Icon">
+          <path d={svgPaths.p27a39400} fill="#5B6574" id="Vector" />
+        </g>
+      </svg>
+    </div>
+  );
+}
+
+function Text2() {
+  return (
+    <div className="content-stretch flex flex-col items-start relative shrink-0" data-name="Text">
+      <p className="[word-break:break-word] font-['Onest:SemiBold',sans-serif] font-semibold leading-[24px] relative shrink-0 text-[#0a1b3a] text-[16px] w-[257px]">Lead for MMC Programme Delivery (OQGA)</p>
+    </div>
+  );
+}
+
+function Container6() {
+  return (
+    <div className="col-1 content-stretch flex gap-[10px] items-center justify-self-stretch relative row-3 self-stretch shrink-0" data-name="Container">
+      <Icon3 />
+      <Text2 />
+    </div>
+  );
+}
+
+function Icon4() {
+  return (
+    <div className="relative shrink-0 size-[18px]" data-name="Icon">
+      <svg className="absolute block inset-0 size-full" fill="none" height="18" preserveAspectRatio="none" viewBox="0 0 18 18" width="18">
+        <g id="Icon">
+          <path d={svgPaths.p27a39400} fill="#5B6574" id="Vector" />
+        </g>
+      </svg>
+    </div>
+  );
+}
+
+function Text3() {
+  return (
+    <div className="content-stretch flex flex-col items-start relative shrink-0" data-name="Text">
+      <p className="[word-break:break-word] font-['Onest:SemiBold',sans-serif] font-semibold leading-[24px] relative shrink-0 text-[#0a1b3a] text-[16px] w-[257px]">{`Corrections, Health & Aviation Expert`}</p>
+    </div>
+  );
+}
+
+function Container7() {
+  return (
+    <div className="col-1 content-stretch flex gap-[10px] items-center justify-self-stretch relative row-4 self-stretch shrink-0" data-name="Container">
+      <Icon4 />
+      <Text3 />
+    </div>
+  );
+}
+
+function Container3() {
+  return (
+    <div className="gap-x-[12px] gap-y-[12px] grid grid-cols-[_285px] grid-rows-[____24px_48px_48px_48px] relative shrink-0 w-[285px]" data-name="Container">
+      <Container4 />
+      <Container5 />
+      <Container6 />
+      <Container7 />
+    </div>
+  );
+}
+
+function ContainerMargin1() {
+  return (
+    <div className="content-stretch flex flex-col items-start pb-[28px] pt-[24px] relative shrink-0" data-name="Container:margin">
+      <Container3 />
+    </div>
+  );
+}
+
+function Icon5() {
+  return (
+    <div className="relative shrink-0 size-[18px]" data-name="Icon">
+      <svg className="absolute block inset-0 size-full" fill="none" height="18" preserveAspectRatio="none" viewBox="0 0 18 18" width="18">
+        <g id="Icon">
+          <path d={svgPaths.p27a46300} fill="#5B6574" id="Vector" />
+        </g>
+      </svg>
+    </div>
+  );
+}
+
+function Text4() {
+  return (
+    <div className="content-stretch flex flex-col items-start relative shrink-0" data-name="Text">
+      <p className="[word-break:break-word] font-['Onest:Bold',sans-serif] font-bold leading-[24px] relative shrink-0 text-[#0a1b3a] text-[16px] whitespace-nowrap">0436 603 061</p>
+    </div>
+  );
+}
+
+function Link() {
+  return (
+    <div className="absolute content-stretch flex gap-[8px] items-center left-0 min-h-[44px] py-[8px] top-[22px]" data-name="Link">
+      <Icon5 />
+      <Text4 />
+    </div>
+  );
+}
+
+function Icon6() {
+  return (
+    <div className="relative shrink-0 size-[18px]" data-name="Icon">
+      <svg className="absolute block inset-0 size-full" fill="none" height="18" preserveAspectRatio="none" viewBox="0 0 18 18" width="18">
+        <g id="Icon">
+          <path d={svgPaths.p2aa90880} fill="#5B6574" id="Vector" />
+        </g>
+      </svg>
+    </div>
+  );
+}
+
+function Text5() {
+  return (
+    <div className="content-stretch flex flex-col items-start relative shrink-0" data-name="Text">
+      <p className="[word-break:break-word] font-['Onest:Bold',sans-serif] font-bold leading-[24px] relative shrink-0 text-[#0a1b3a] text-[16px] whitespace-nowrap">dlansdell@constructqld.com.au</p>
+    </div>
+  );
+}
+
+function Link1() {
+  return (
+    <div className="absolute content-stretch flex gap-[8px] items-center left-0 min-h-[44px] py-[8px] top-[88px]" data-name="Link">
+      <Icon6 />
+      <Text5 />
+    </div>
+  );
+}
+
+function Text6() {
+  return (
+    <div className="content-stretch flex flex-[138.148_0_0] flex-col items-center min-w-px px-[20px] py-[8px] relative" data-name="Text">
+      <p className="[word-break:break-word] font-['Onest:SemiBold',sans-serif] font-semibold leading-[16.8px] relative shrink-0 text-[14px] text-center text-white whitespace-nowrap">Connect on LinkedIn</p>
+    </div>
+  );
+}
+
+function Icon7() {
+  return (
+    <div className="max-h-[18.399999618530273px] max-w-[18.399999618530273px] relative shrink-0 size-[18.398px]" data-name="Icon">
+      <svg className="absolute block inset-0 size-full" fill="none" height="18.3984" preserveAspectRatio="none" viewBox="0 0 18.3984 18.3984" width="18.3984">
+        <g id="Icon">
+          <path d={svgPaths.p2434d500} fill="white" id="Vector" />
+        </g>
+      </svg>
+    </div>
+  );
+}
+
+function Text7() {
+  return (
+    <div className="flex flex-row items-center self-stretch">
+      <div className="bg-[#061024] border-[rgba(255,255,255,0.3)] border-l border-solid content-stretch flex h-full items-center justify-center relative shrink-0 w-[38px]" data-name="Text">
+        <Icon7 />
+      </div>
+    </div>
+  );
+}
+
+function Link2() {
+  return (
+    <div className="absolute bg-[#0a1b3a] border border-[#0a1b3a] border-solid content-stretch flex items-center justify-center left-0 top-[154px] w-[218.148px]" data-name="Link">
+      <Text6 />
+      <Text7 />
+    </div>
+  );
+}
+
+function Container8() {
+  return (
+    <div className="border-[#eef0f2] border-solid border-t h-[189.797px] relative shrink-0 w-[285px]" data-name="Container">
+      <Link />
+      <Link1 />
+      <Link2 />
+    </div>
+  );
+}
+
+function Container2() {
+  return (
+    <div className="col-1 content-stretch flex flex-col items-start justify-center justify-self-stretch px-[24px] py-[32px] relative row-2 self-stretch shrink-0" data-name="Container">
+      <Paragraph />
+      <Heading />
+      <ContainerMargin />
+      <p className="[word-break:break-word] font-['Onest:Regular',sans-serif] font-normal leading-[26.4px] relative shrink-0 text-[#5b6574] text-[16px] w-[285px]">{`Over 25 years across government and tier-1 delivery, including the QCS portfolio of works — with the institutional insight and senior-level visibility rarely accessible to external advisors. `}</p>
+      <ContainerMargin1 />
+      <Container8 />
+    </div>
+  );
+}
+
+function Container() {
+  return (
+    <div className="bg-white border border-[#eef0f2] border-solid grid grid-cols-[_333px] grid-rows-[__445.98px_826.77px] h-[1274.75px] overflow-clip relative shadow-[0px_10px_30px_0px_rgba(0,0,0,0.05)] shrink-0 w-full" data-name="Container">
+      <Container1 />
+      <Container2 />
+    </div>
+  );
+}
+
+export default function Section() {
+  return (
+    <div className="bg-white content-stretch flex flex-col items-start px-[20px] py-[60px] relative size-full" data-name="Section">
+      <Container />
+    </div>
+  );
+}
